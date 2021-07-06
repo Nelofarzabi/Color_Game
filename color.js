a4f0bf16f43c9b9a2d6d80b1f4f823f8f791e4ef
Nelofar Zabi <Nelofarzabi2000@gmail.com>
2021-07-06 04:32:42 -0700

    Update color.js

--- color.js
+++ color.js
@@ -4,3 +4,9 @@ let colorCircle = document.querySelector('div').addEventListener('mousemove', ch
 function changeColor(e) {
     let x = document.body.style.backgroundColor = `rgb(${e.offsetX} ,${e.offsetY},20)`;
 }
+let mouseOut = document.querySelector('div').addEventListener('mouseout', unChangeAble);
+
+function unChangeAble(e) {
+    let x = document.body.style.backgroundColor = 'white';
+
+}
